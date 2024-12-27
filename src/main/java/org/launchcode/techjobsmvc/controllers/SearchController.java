a4600823cfc22e41817e091cdc2b58230cbc6f05
Angie <angiepolitte@gmail.com>
2024-12-27 07:03:23 -0600

    TestTaskThree Part 1 Passed

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -2,8 +2,7 @@ package org.launchcode.techjobsmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -22,6 +21,14 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("searchTerm", searchTerm);
+        return "results";
+    }
 
 }
 
